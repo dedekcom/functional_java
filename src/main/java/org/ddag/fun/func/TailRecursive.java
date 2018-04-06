554e11ac42803e7de90020b199f5f165c754ea51
DominikDa <dominikda@advaoptical.com>
2018-04-06 22:58:28 +0200

    tail rec refactor

--- src/main/java/org/ddag/fun/func/TailRecursive.java
+++ src/main/java/org/ddag/fun/func/TailRecursive.java
@@ -8,7 +8,9 @@ package org.ddag.fun.func;
 
 
 import org.ddag.fun.tuple.FunTuple;
-import org.ddag.fun.tuple.Tuple6;
+import org.ddag.fun.tuple.Tuple2;
+import org.ddag.fun.tuple.Tuple3;
+import org.ddag.fun.tuple.Tuple4;
 
 import java.util.function.Function;
 
@@ -16,52 +18,50 @@ import java.util.function.Function;
 public interface TailRecursive {
 
   static <R> R tailRec(R initialResult, Function<R, FunTuple> fun) {
-    Tuple6<Boolean, R, Object, Object, Object, Object> result = new Tuple6<>(true, initialResult, null, null, null, null);
+    Tuple2<Boolean, R> result = new Tuple2<>(true, initialResult);
     boolean continueLoop;
     do {
       FunTuple r = fun.apply(result._2());
       continueLoop = (Boolean)r._id(0);
       if (continueLoop) {
-        result = (Tuple6<Boolean, R, Object, Object, Object, Object>)r;
+        result = (Tuple2<Boolean, R>)r;
       }
     } while (continueLoop);
     return result._2();
   }
 
   static <R, T> R tailRec(R initialResult, T arg1, RecFun2<R, T> fun) {
-    Tuple6<Boolean, R, T, Object, Object, Object> result = new Tuple6<>(true, initialResult, arg1, null, null, null);
+    Tuple3<Boolean, R, T> result = new Tuple3<>(true, initialResult, arg1);
     boolean continueLoop;
     do {
       FunTuple r = fun.apply(result._2(), result._3());
       continueLoop = (Boolean)r._id(0);
       if (continueLoop) {
-        result = (Tuple6<Boolean, R, T, Object, Object, Object>)r;
+        result = (Tuple3<Boolean, R, T>)r;
       }
     } while (continueLoop);
     return result._2();
   }
 
   static <R, T1, T2> R tailRec(R initialResult, T1 arg1, T2 arg2, RecFun3<R, T1, T2> fun) {
-    Tuple6<Boolean, R, T1, T2, Object, Object> result = new Tuple6<>(true, initialResult, arg1, arg2, null, null);
+    Tuple4<Boolean, R, T1, T2> result = new Tuple4<>(true, initialResult, arg1, arg2);
     boolean continueLoop;
     do {
       FunTuple r = fun.apply(result._2(), result._3(), result._4());
       continueLoop = (Boolean)r._id(0);
       if (continueLoop) {
-        result = (Tuple6<Boolean, R, T1, T2, Object, Object>)r;
+        result = (Tuple4<Boolean, R, T1, T2>)r;
       }
     } while (continueLoop);
     return result._2();
   }
 
-  static <R> FunTuple Return(R result) {    return new Tuple6<>(false, result, null, null, null, null);  }
+  static <R> FunTuple Return(R result) {    return new Tuple2<>(false, result);  }
 
-  static <R> FunTuple Continue(R result) {    return new Tuple6<>(true, result, null, null, null, null);  }
+  static <R> FunTuple Continue(R result) {    return new Tuple2<>(true, result);  }
 
-  static <R, T> FunTuple Continue(R result, T arg1) {  return new Tuple6<>(true, result, arg1, null, null, null);  }
+  static <R, T> FunTuple Continue(R result, T arg1) {  return new Tuple3<>(true, result, arg1);  }
 
-  static <R, T1, T2> FunTuple Continue(R result, T1 arg1, T2 arg2) {
-    return new Tuple6<>(true, result, arg1, arg2, null, null);
-  }
+  static <R, T1, T2> FunTuple Continue(R result, T1 arg1, T2 arg2) {    return new Tuple4<>(true, result, arg1, arg2);  }
 
 }
